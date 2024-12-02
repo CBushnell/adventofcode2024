495479f33b6968ac288360ce371fff58288ced02
Connor Bushnell <connorbushnell@connors-mbp-4.mynetworksettings.com>
2024-12-02 14:30:09 -0500

    completed day 1 beginning day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn read_file_to_vect(filepath: &str) -> Vec<String> {
+    let contents: Result<String, io::Error> = fs::read_to_string(filepath);
+    contents.expect("Didn't load correctly").lines().map(|line| line.to_string()).collect()
+}
